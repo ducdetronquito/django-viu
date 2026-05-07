1285566d0645c2f3e4212833f0641d74e2a9dc0a
ducdetronquito <g.paulet@smartway.ai>
2026-05-07 10:17:07 +0200

    feat: First experiments

---
+++ tests/test_viu.py
@@ -0,0 +1,147 @@
+from http import HTTPStatus
+
+import pytest
+from django.http import JsonResponse
+from django.test import Client, RequestFactory, SimpleTestCase, override_settings
+from django.urls import path
+from pydantic import BaseModel, ValidationError
+
+from viu import Json, Path, Query, Router
+
+
+class QueryParams(BaseModel):
+    id: int
+    name: str
+
+
+class PathParams(BaseModel):
+    age: int
+    name: str
+
+
+class Payload(BaseModel):
+    name: str
+    age: int
+
+
+router = Router()
+
+
+@router.get("/query")
+def get_from_query_params(params: Query[QueryParams]) -> JsonResponse:
+    return JsonResponse(status=200, data={"id": params.id, "name": params.name})
+
+
+@router.get("/path/<name>/with/<age>")
+def get_from_path_params(params: Path[PathParams]) -> JsonResponse:
+    return JsonResponse(status=200, data={"age": params.age, "name": params.name})
+
+
+@router.post("/post")
+def post_stuff() -> JsonResponse:
+    return JsonResponse({}, status=200)
+
+
+@router.post("/post-json")
+def post_json(payload: Json[Payload]) -> JsonResponse:
+    return JsonResponse({"name": payload.name, "age": payload.age}, status=200)
+
+
+@router.route(methods={"GET", "POST"}, path="/route")
+def get_from_route() -> JsonResponse:
+    return JsonResponse({}, status=200)
+
+
+urlpatterns = [path("", router.urls)]
+
+
+@override_settings(ROOT_URLCONF="tests.test_viu")
+class TestRouter(SimpleTestCase):
+    def test_get(self):
+        response = Client().get("/query?id=2&name=joe")
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == {"id": 2, "name": "joe"}
+
+    def test_post(self):
+        response = Client().post("/post")
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == {}
+
+    def test_route(self):
+        response = Client().get("/route")
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == {}
+
+        response = Client().post("/route")
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == {}
+
+        response = Client().patch("/route")
+        assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+        assert response.json() == {}
+
+    def test_with_path_params(self):
+        response = Client().get("/path/averell/with/33")
+        assert response.status_code == HTTPStatus.OK
+
+    def test_not_found(self):
+        response = Client().get("/not-found")
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        # TODO: 404 are return by django itself, therefore the
+        # content type is html.
+        # Should we replace the django routing system entirely (very meh) or
+        # just let the user handle it with handler404 ?
+        # Cf:
+        # - https://docs.djangoproject.com/en/5.2/topics/http/views/#the-http404-exception
+        # - https://docs.djangoproject.com/en/5.2/topics/http/views/#customizing-error-views
+
+
+class TestQuery(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.request_factory = RequestFactory()
+
+    def test_works(self):
+        request = self.request_factory.get("/?id=1&name=toto")
+
+        response = get_from_query_params(request)
+
+        assert response.status_code == 200
+        assert response.content == b'{"id": 1, "name": "toto"}'
+
+    def test_fails(self):
+        request = self.request_factory.get("/?id=toto&name=1")
+
+        with pytest.raises(ValidationError):
+            get_from_query_params(request)
+
+
+@override_settings(ROOT_URLCONF="tests.test_viu")
+class TestJson(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.request_factory = RequestFactory()
+
+    def test_works(self):
+        request = self.request_factory.post(
+            "/post-json",
+            data={"name": "Averell", "age": 30},
+            content_type="application/json",
+        )
+
+        response = post_json(request)
+
+        assert response.status_code == 200
+        assert response.content == b'{"name": "Averell", "age": 30}'
+
+    def test_fails(self):
+        request = self.request_factory.post(
+            "/post-json",
+            data={},
+            content_type="application/json",
+        )
+
+        with pytest.raises(ValidationError):
+            post_json(request)
---
+++ viu/__init__.py
@@ -0,0 +1,117 @@
+from http import HTTPStatus
+from typing import (
+    Annotated,
+    Callable,
+    Concatenate,
+    Literal,
+    Protocol,
+    cast,
+    get_args,
+    get_origin,
+    get_type_hints,
+)
+
+from django.http import HttpResponse
+from django.http.request import HttpRequest
+from django.http.response import JsonResponse
+from django.urls import URLPattern, URLResolver
+from django.urls import path as django_path
+from pydantic import BaseModel
+from typing_extensions import Any
+
+type Path[T: BaseModel] = Annotated[T, "from-path-params"]
+type Query[T: BaseModel] = Annotated[T, "from-query-params"]
+type Json[T: BaseModel] = Annotated[T, "from-json-payload"]
+
+
+class Extractor[T](Protocol):
+    def from_request(self, request: HttpRequest) -> T: ...
+
+
+class PathExtractor[T: BaseModel]:
+    def __init__(self, output_type: type[T]) -> None:
+        self.output_type = output_type
+
+    def from_request(self, request: HttpRequest) -> T:
+        resolver_match = request.resolver_match
+        assert resolver_match is not None
+        # NB: `captured_kwargs` is not defined as a ResolverMatch field in django-types
+        captured_kwargs = cast(dict[str, Any], resolver_match.captured_kwargs)  # pyright: ignore[reportAttributeAccessIssue]
+        return self.output_type.model_validate(captured_kwargs)
+
+
+class QueryExtractor[T: BaseModel]:
+    def __init__(self, output_type: type[T]) -> None:
+        self.output_type = output_type
+
+    def from_request(self, request: HttpRequest) -> T:
+        return self.output_type(**request.GET.dict())
+
+
+class JsonExtractor[T: BaseModel]:
+    def __init__(self, output_type: type[T]) -> None:
+        self.output_type = output_type
+
+    def from_request(self, request: HttpRequest) -> T:
+        return self.output_type.model_validate_json(request.body)
+
+
+type DjangoView = Callable[Concatenate[HttpRequest, ...], HttpResponse]
+type Viu[**P] = Callable[P, HttpResponse]
+type HttpMethod = Literal["GET"] | Literal["POST"]
+
+
+class Router:
+    def __init__(self) -> None:
+        self._views = list[URLPattern | URLResolver]()
+
+    def get(self, path: str):
+        return self.route(methods={"GET"}, path=path)
+
+    def post(self, path: str):
+        return self.route(methods={"POST"}, path=path)
+
+    def route(self, methods: set[HttpMethod], path: str):
+        def wrapper[**P](
+            viu: Viu[P],
+        ) -> DjangoView:
+            print(viu.__name__)
+            annotations = get_type_hints(viu, include_extras=True)
+            _return_type = annotations.pop("return")
+            parameters_types = annotations
+
+            extractors = dict[str, Extractor[Any]]()
+            for parameter_name, parameter_type in parameters_types.items():
+                alias = get_origin(parameter_type)
+                type_args = get_args(parameter_type)[0]
+                metadata = alias.__value__.__metadata__[0]
+                if metadata == "from-path-params":
+                    extractors[parameter_name] = PathExtractor(type_args)
+                elif metadata == "from-query-params":
+                    extractors[parameter_name] = QueryExtractor(type_args)
+                elif metadata == "from-json-payload":
+                    extractors[parameter_name] = JsonExtractor(type_args)
+                print(f"\tArgument name => {parameter_name}")
+                print(f"\tType name     => {alias.__name__}")
+                print(f"\tType args     => {type_args}")
+                print(f"\tType metadata =>{metadata}")
+
+            def django_view(request: HttpRequest, *args: P.args, **kwargs: P.kwargs) -> HttpResponse:
+                if request.method not in methods:
+                    return JsonResponse({}, status=HTTPStatus.METHOD_NOT_ALLOWED)
+
+                assert isinstance(request, HttpRequest)
+                inputs = dict[str, Any]()
+                for argument_name, _ in annotations.items():
+                    inputs[argument_name] = extractors[argument_name].from_request(request)
+                return viu(**inputs)
+
+            _path = path.removeprefix("/")
+            self._views.append(django_path(_path, django_view))
+            return django_view
+
+        return wrapper
+
+    @property
+    def urls(self) -> tuple[list[URLResolver | URLPattern], str, str]:
+        return (self._views, "", "")
