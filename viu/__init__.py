c4db18f4bf9c315bfb1996e9643b60b4e5184a69
ducdetronquito <g.paulet@smartway.ai>
2026-05-17 19:24:01 +0200

    refactor: Remove generic & paramspec where it does not help at all

--- viu/__init__.py
+++ viu/__init__.py
@@ -57,8 +57,9 @@ class JsonExtractor[T: BaseModel]:
 
 
 type DjangoView = Callable[Concatenate[HttpRequest, ...], HttpResponse]
-type Viu[**P] = Callable[P, HttpResponse]
+type AnyHandler = Callable[..., HttpResponse]
 type HttpMethod = Literal["GET"] | Literal["POST"]
+type Extractors = dict[str, Extractor[BaseModel]]
 
 
 class Router:
@@ -72,44 +73,51 @@ class Router:
         return self.route(methods={"POST"}, path=path)
 
     def route(self, path: str, methods: set[HttpMethod] | None = None):
-        def wrapper[**P](viu: Viu[P]) -> DjangoView:
-            print(viu.__name__)
-            annotations = get_type_hints(viu, include_extras=True)
-            _return_type = annotations.pop("return")
-            parameters_types = annotations
 
-            extractors = dict[str, Extractor[Any]]()
-            for parameter_name, parameter_type in parameters_types.items():
-                alias = get_origin(parameter_type)
-                assert alias is not None
-                type_args = get_args(parameter_type)[0]
-                metadata = alias.__value__.__metadata__[0]
-                if metadata == "from-path-params":
-                    extractors[parameter_name] = PathExtractor(type_args)
-                elif metadata == "from-query-params":
-                    extractors[parameter_name] = QueryExtractor(type_args)
-                elif metadata == "from-json-payload":
-                    extractors[parameter_name] = JsonExtractor(type_args)
-                print(f"\tArgument name => {parameter_name}")
-                print(f"\tType name     => {alias.__name__}")
-                print(f"\tType args     => {type_args}")
-                print(f"\tType metadata =>{metadata}")
+        def wrapper[**P](handler: AnyHandler) -> DjangoView:
+            print(handler.__name__)
+            extractors = self._get_extractors(handler)
 
             def django_view(request: HttpRequest, *args: P.args, **kwargs: P.kwargs) -> HttpResponse:
                 if methods and request.method not in methods:
                     return JsonResponse({}, status=HTTPStatus.METHOD_NOT_ALLOWED)
 
-                assert isinstance(request, HttpRequest)
-                inputs = dict[str, Any]()
-                for argument_name, _ in annotations.items():
-                    inputs[argument_name] = extractors[argument_name].from_request(request)
-                return viu(*args, **inputs)
+                inputs = {
+                    argument_name: extractors[argument_name].from_request(request)
+                    for argument_name, extractor in extractors.items()
+                }
+                return handler(*args, **inputs)
 
             _path = path.removeprefix("/")
             self._views.append(django_path(_path, django_view))
             return django_view
 
         return wrapper
+
+    def _get_extractors(self, handler: AnyHandler) -> Extractors:
+        annotations = get_type_hints(handler, include_extras=True)
+        _return_type = annotations.pop("return")
+        parameters_types = annotations
+
+        extractors: Extractors = {}
+        for parameter_name, parameter_type in parameters_types.items():
+            alias = get_origin(parameter_type)
+            assert alias is not None
+            type_args = get_args(parameter_type)[0]
+            assert issubclass(type_args, BaseModel)
+            metadata = alias.__value__.__metadata__[0]
+            if metadata == "from-path-params":
+                extractors[parameter_name] = PathExtractor(type_args)
+            elif metadata == "from-query-params":
+                extractors[parameter_name] = QueryExtractor(type_args)
+            elif metadata == "from-json-payload":
+                extractors[parameter_name] = JsonExtractor(type_args)
+            print(f"\tArgument name => {parameter_name}")
+            print(f"\tType name     => {alias.__name__}")
+            print(f"\tType args     => {type_args}")
+            print(f"\tType metadata =>{metadata}")
+
+        return extractors
 
     @property
     def urls(self) -> tuple[list[URLResolver | URLPattern], str, str]:
